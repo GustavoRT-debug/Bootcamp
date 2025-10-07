ae33b02669bedbac7e262134e1385d56c8aa5ebe
GustavoTorres <88936210+GustavoRT-debug@users.noreply.github.com>
2025-10-07 14:49:47 -0300

    Update main.java

---
+++ Aula 13/srs/main.java
@@ -0,0 +1,22 @@
+package srs;
+
+public class main {
+    public static void main(String[] args){
+        int i;
+        int I;
+        
+        i=5;
+        I=10;
+
+        final int j=10;
+        
+        int qualidadeProduto=50;
+
+        System.out.println((i));
+        System.out.println((I));
+        System.out.println((qualidadeProduto));
+
+    }
+    
+//
+}
