005ce9fd9ca720789ead15b5081a942d90ed4a41
GustavoTorres <88936210+GustavoRT-debug@users.noreply.github.com>
2025-10-07 14:50:00 -0300

    Update main2.java

---
+++ Aula 13/srs/main2.java
@@ -0,0 +1,15 @@
+package srs;
+
+public class main2 {
+    public static void main(String[] args){
+        byte b1=10;
+        byte b2=20;
+
+        short s1=2000;
+        
+        int i2=2000;
+
+    }
+
+    //
+}
