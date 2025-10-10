6f233deb54a2bbe2a9a28a1e965add54c0399d56
GustavoTorres <88936210+GustavoRT-debug@users.noreply.github.com>
2025-10-10 18:00:08 -0300

    Update array.java

---
+++ Aula 14/src/array.java
@@ -0,0 +1,17 @@
+package src;
+
+public class array {
+    public static void main(String[] args) {
+        int[] vetor={0,-5,15,50,8,4};
+        
+        int count=0;
+        while(count<vetor.length-1){
+            System.out.println(vetor[count]);
+            count++;
+    
+        }
+        for(int i=(vetor.length-1);i==0;i--){
+            System.out.println(vetor[i]+"");
+        }
+    }
+}
