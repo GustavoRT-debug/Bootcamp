4419a66e4f8e2eec50789a71c8538915106cfd58
GustavoTorres <88936210+GustavoRT-debug@users.noreply.github.com>
2025-10-10 18:00:24 -0300

    Update ex.java

---
+++ Aula 14/src/ex.java
@@ -0,0 +1,26 @@
+package src;
+
+import java.util.Scanner;
+
+public class ex {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while (true) {
+            System.out.println("Nome: ");
+            nome=scan.next();
+            if(nome.equals("0")) break;
+            
+            System.out.println("Idade: ");
+            idade=scan.nextInt();
+            
+        }
+        
+        
+        
+    }
+    
+}
